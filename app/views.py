1c34958c218389dbde71e23093ea08de9c8b798a
gzMichael <1960809@qq.com>
2017-02-22 01:00:57 +0800

    update views

--- app/views.py
+++ app/views.py
@@ -13,34 +13,48 @@ from app import api_openweathermap as api_owm
 
 class QueryForm(FlaskForm):
     city_name = StringField('需要查询天气的城市名称', validators=[DataRequired()])
+    temp_unit = SelectField('温度单位', choices=[('1','℃'),('2','℉')])
     submit = SubmitField('查询')
     
 def check_zh_or_en(check_str):
-    for ch in city_name:
+    for ch in check_str:
             if u'\u4e00' <= ch <= u'\u9fff':
                 return True
     return False
-                
+    
+def convert_c_to_f(str1):
+        """Convert Celsius to Fahrenheit
+        Args:
+            A Celsius temperature in string
+        Returns:
+            A Fahrenheit temperature in string
+        """
+        temp1 = float(str1) * 1.8 +32
+        str2 = str("%2.f"%temp1)
+        print(str1,str2)
+        return str2
+       
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     ip_str = request.remote_addr
     province_str, city_str = locationquery(ip_str)
     location_str = province_str + city_str
-    dict_weather_forecast = {}
+    dict_weather = {}
     queryform = QueryForm()
     if queryform.city_name.data:
         city_name = queryform.city_name.data
+        temp_unit = queryform.temp_unit.data
         #根据是否输入中文，调用不同的接口
         if check_zh_or_en(city_name):
-            dict_weather_forecast = api_tp.get_weather_forecast(city_name)
+            dict_weather = api_tp.get_weather_forecast(city_name)
             api_str = '心知天气'
         else:
-            dict_weather_forecast = api_owm.get_weather_forecast(city_name)
+            dict_weather = api_owm.get_weather_forecast(city_name)
             api_str = 'OpenWeatherMap'
         #如果接口有数据返回
-        if dict_weather_forecast:
-            if dict_weather_forecast['status_code'] == '200':
+        if dict_weather:
+            if dict_weather['status_code'] == '200':
                 history = History()
                 history.cityname = city_name
                 history.ip = ip_str
@@ -49,14 +63,28 @@ def index():
                 print(dt.strftime("%Y-%m-%d %H:%M:%S"))
                 history.location = location_str
                 history.api = api_str
-                history.weather = dict_weather_forecast['weather_d0']
-                history.weathercode = dict_weather_forecast['weather_code_d0']
-                history.tempmin = dict_weather_forecast['temp_min_d0']
-                history.tempmax = dict_weather_forecast['temp_max_d0']
+                history.weather = dict_weather['weather_d0']
+                history.weathercode = dict_weather['weather_code_d0']
+                history.tempmin = dict_weather['temp_min_d0']
+                history.tempmax = dict_weather['temp_max_d0']
                 db.session.add(history)
                 db.session.commit()
+                if temp_unit == '2':
+                    print(dict_weather['temp_min_d0'])
+                    a = dict_weather['temp_min_d0']
+                    dict_weather['temp_min_d0'] = convert_c_to_f(dict_weather['temp_min_d0'])
+                    print(dict_weather['temp_min_d0'])
+                    b = dict_weather['temp_min_d0']
+                    dict_weather['temp_max_d0'] = convert_c_to_f(dict_weather['temp_max_d0'])
+                    dict_weather['temp_min_d1'] = convert_c_to_f(dict_weather['temp_min_d1'])
+                    dict_weather['temp_max_d1'] = convert_c_to_f(dict_weather['temp_max_d1'])
+                    dict_weather['temp_min_d2'] = convert_c_to_f(dict_weather['temp_min_d2'])
+                    dict_weather['temp_max_d2'] = convert_c_to_f(dict_weather['temp_max_d2'])
+                    temp_unit_str = '℉'
+                else:
+                    temp_unit_str = '℃'
         return render_template('index.html', form=queryform, city_name=city_name, api=api_str,
-            ip_str=ip_str, location_str=location_str, dict_weather_forecast=dict_weather_forecast)
+            weather=dict_weather, unit=temp_unit_str)
     else:
         print(queryform.city_name.data)
         return render_template('index.html', form=queryform, location_str=location_str)
