ca02b364d520309da9addfa34f929b93e499fae8
gzmichael <1960809@qq.com>
2017-09-18 13:34:12 +0800

    update init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-  
 
 from app import app
-from flask_bootstrap import Bootstrap
 
 if __name__ == '__main__':
     app.debug = True
